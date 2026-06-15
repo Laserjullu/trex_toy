5d68ab4c7b49883f4fdbc2f980db725c60d4d11a
Julius Reitlinger <julius.reitlinger@gmail.com>
2026-06-15 11:10:49 +0200

    added automated test and changed "assert" to fit unittest "assertEqual"

--- Tests/test_bitvector.py
+++ Tests/test_bitvector.py
@@ -7,17 +7,17 @@ class test_bitvector(unittest.TestCase):
     def test_bitvector(self):
         B = DummyBitvector([1, 0, 1, 1, 0, 1])
     
-        assert B.rank(2, 1) == 2 
-        assert B.rank(4, 0) == 2  
+        self.assertEqual(B.rank(2, 1), 2) 
+        self.assertEqual(B.rank(4, 0), 2)  
     
-        assert B.select(1, 1) == 0 
-        assert B.select(2, 0) == 4
+        self.assertEqual(B.select(1, 1), 0) 
+        self.assertEqual(B.select(2, 0), 4)
     
-        assert B.predecessor(3, 1) == 3 
-        assert B.predecessor(4, 0) == 4
+        self.assertEqual(B.predecessor(3, 1), 3) 
+        self.assertEqual(B.predecessor(4, 0), 4)
     
-        assert B.successor(2, 1) == 3 
-        assert B.successor(4, 1) == 5
+        self.assertEqual(B.successor(2, 1), 3) 
+        self.assertEqual(B.successor(4, 1), 5)
 
 if __name__ == '__main__':
     unittest.main()
--- Tests/test_louds.py
+++ Tests/test_louds.py
@@ -7,17 +7,16 @@ class test_louds(unittest.TestCase):
     
         louds_tree = DummyLouds(root)
     
-        assert louds_tree.degree(1) == 2
-        assert louds_tree.degree(2) == 0
+        self.assertEqual(louds_tree.degree(1), 2)
+        self.assertEqual(louds_tree.degree(2), 0)
 
-        assert louds_tree.child(1, 1) == 2
-        assert louds_tree.child(1, 2) == 3
+        self.assertEqual(louds_tree.child(1, 1), 2)
+        self.assertEqual(louds_tree.child(1, 2), 3)
     
-        assert louds_tree.parent(2) == 1
-        assert louds_tree.parent(3) == 1
-        assert louds_tree.child_rank(2) == 1
-        assert louds_tree.child_rank(3) == 2
-        print("Passed :)")
+        self.assertEqual(louds_tree.parent(2), 1)
+        self.assertEqual(louds_tree.parent(3), 1)
+        self.assertEqual(louds_tree.child_rank(2), 1)
+        self.assertEqual(louds_tree.child_rank(3), 2)
 
 if __name__ == '__main__':
     unittest.main()
